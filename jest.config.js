794081be54c0d1a100de7f35b00dbbb56cc9b1e5
Jon Corbin <jon.corbin8@gmail.com>
2022-12-12 01:08:57 -0500

    multiple changes
    - lint
    - add coverage thresholds

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,20 @@
 module.exports = {
   collectCoverage: true,
-  collectCoverageFrom: ["**/*.js"],
+  collectCoverageFrom: ['**/*.js'],
   verbose: true,
   coveragePathIgnorePatterns: [
-    "node_modules",
-    "test",
-    "coverage",
-    "jest.config.js",
-    "index.js",
+    'node_modules',
+    'test',
+    'coverage',
+    'jest.config.js',
+    'index.js'
   ],
-};
+  coverageThreshold: {
+    global: {
+      branches: 90,
+      functions: 90,
+      lines: 90,
+      statements: 90
+    }
+  }
+}
